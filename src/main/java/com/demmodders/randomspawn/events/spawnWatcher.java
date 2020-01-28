a74aa961c7a4673b8eccc076a72ef017023343bb
Jacob <jtljac2@gmail.com>
2020-01-28 14:56:29 +0000

    more stuff

--- src/main/java/com/demmodders/randomspawn/events/spawnWatcher.java
+++ src/main/java/com/demmodders/randomspawn/events/spawnWatcher.java
@@ -1,8 +1,7 @@
 package com.demmodders.randomspawn.events;
 
-import com.demmodders.factions.faction.FactionManager;
-import com.demmodders.factions.util.FactionConfig;
 import com.demmodders.randomspawn.RandomSpawn;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,9 +12,15 @@ public class spawnWatcher {
     @SubscribeEvent
     public static void playerJoin(PlayerEvent.PlayerLoggedInEvent e){
         NBTTagCompound test = e.player.getEntityData();
-        if (test.getSize() == 0){
-            test.setBoolean("joined", true);
+        if (test.hasKey("player.PERSISTED_NBT_TAG")) {
+            if (!test.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG).getBoolean(RandomSpawn.MODID + ".joined")) {
+                test.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG).setBoolean(RandomSpawn.MODID + ".joined", true);
+                // TODO: Teleport
+            }
+        } else {
+            test.setTag(EntityPlayer.PERSISTED_NBT_TAG, new NBTTagCompound());
+            test.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG).setBoolean(RandomSpawn.MODID + ".joined", true);
+            // TODO: Teleport
         }
-        FactionManager.getInstance()
     }
 }
