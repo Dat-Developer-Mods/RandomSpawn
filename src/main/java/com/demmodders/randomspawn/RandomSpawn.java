6b12d94a2ed872024e18b452bb5afe8d4985b85b
Jacob <jtljac2@gmail.com>
2020-08-16 02:11:01 +0100

    Updated to forgegradle 3

--- src/main/java/com/demmodders/randomspawn/RandomSpawn.java
+++ src/main/java/com/demmodders/randomspawn/RandomSpawn.java
@@ -6,12 +6,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = RandomSpawn.MODID, name = RandomSpawn.NAME,  version = RandomSpawn.VERSION, acceptableRemoteVersions  = "*", dependencies = "required-after:datmoddingapi@[1.0.1,)")
+@Mod(modid = RandomSpawn.MODID, name = RandomSpawn.NAME,  version = RandomSpawn.VERSION, acceptableRemoteVersions  = "*", dependencies = "required-after:datmoddingapi@[1.1.0,)")
 public class RandomSpawn
 {
     public static final String MODID = "datrandomspawn";
     public static final String NAME = "Random Spawn";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
     public static final String MC_VERSION = "[1.12.2]";
 
     @Mod.EventHandler
